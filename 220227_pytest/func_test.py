002eddfc56b29d31f3ec2e2c327f0c751a097bb4
lhj9520 <gw9520@naver.com>
2022-03-19 12:58:44 +0900

    how to use git

--- 220227_pytest/func_test.py
+++
@@ -1,2 +0,0 @@
-def func():
-    return "func 함수가 호출되었습니다."
--- 220227_pytest/test.py
+++
@@ -1,8 +0,0 @@
-def plus(a,b):
-    return a+b
-def minus(a,b):
-    return a-b
-def mul(a,b):
-    return a*b
-def div(a,b):
-    return a/b
